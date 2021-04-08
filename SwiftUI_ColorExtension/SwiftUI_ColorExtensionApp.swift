1e719c5d144be16786b05b2e208c43adb48a12f5
SeonJungKim <ksj083895@gmail.com>
2021-04-08 23:18:12 +0500

    Initial Commit

---
+++ SwiftUI_ColorExtension/SwiftUI_ColorExtensionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ColorExtensionApp.swift
+//  SwiftUI_ColorExtension
+//
+//  Created by 김선중 on 2021/04/08.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ColorExtensionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
